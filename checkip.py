c74adbc9004c5437e04533c33d664d89a8ad4cf7
sapikwaidi <sapikwaidi@gmail.com>
2023-08-04 17:25:17 +0700

    add checkip.py

---
+++ checkip.py
@@ -0,0 +1,15 @@
+import socket
+import requests
+import pprint
+import json
+
+hostname = input('Enter a domain name: ')
+ip_address = socket.gethostbyname(hostname)
+
+request_url ='https://geolocation-db.com/jsonp/'+ip_address
+response = requests.get(request_url)
+geolocation = response.content.decode()
+geolocation = geolocation.split("(")[1].strip(")")
+geolocation = json.loads(geolocation)
+for k,v in geolocation.items():
+	pprint.pprint(str(k)+ ':' + str(v))
